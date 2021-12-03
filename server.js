489a23001bda8a85f3aa5d9f22b509784976e843
5kaz <51829343+5kaz@users.noreply.github.com>
2021-12-03 17:25:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 var port = 5000;
 
+
 var server = http.createServer(function (req, res) {   //create web server
     if (req.url == '/') { //handle requests
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
